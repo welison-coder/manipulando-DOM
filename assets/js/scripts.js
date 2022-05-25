25ae62f804b0c585a6c5a75f39c8dd557352ade9
welison-coder <wellisson.marques@tc.df.gov.br>
2022-05-25 17:30:13 -0300

    subindo dark mode

---
+++ assets/js/scripts.js
@@ -0,0 +1,32 @@
+function chengeMode(){
+	changeClasses();
+	chengeText();
+}
+
+function changeClasses(){
+	button.classList.toggle(darkModeClass);
+	h1.classList.toggle(darkModeClass);
+	body.classList.toggle(darkModeClass);
+	footer.classList.toggle(darkModeClass);
+}
+
+function chengeText(){
+	const lightMode = 'Light Mode';
+	const darkMode = 'Dark Mode';
+	if(body.classList.contains(darkModeClass)){
+		button.innerHTML = lightMode;
+		h1.innerHTML = darkMode + ' On';
+		return;
+	}
+	button.innerHTML = darkMode;
+	h1.innerHTML = lightMode + ' On';
+	
+}
+
+const darkModeClass = 'dark-mode' 
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+button.addEventListener('click', chengeMode)
